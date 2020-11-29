e639ae996bef061ff1d0dbcb1b60bf2b49ec22bb
FranciscoMendes10866 <avizinhadochico@gmail.com>
2020-11-29 23:27:01 +0100

    updated Patch ✏️

--- backend/articles/PatchArticle.go
+++ backend/articles/PatchArticle.go
@@ -1,6 +1,8 @@
 package articles
 
 import (
+	"strconv"
+
 	"github.com/FranciscoMendes10866/vissay/backend/config"
 	"github.com/FranciscoMendes10866/vissay/backend/models"
 	"github.com/gofiber/fiber/v2"
@@ -19,5 +21,12 @@ func PatchArticle(c *fiber.Ctx) error {
 	if patch == nil {
 		return c.SendStatus(500)
 	}
-	return c.SendStatus(200)
+	convertToInt, _ := strconv.Atoi(articleID)
+	response := resStruct{
+		ID:      uint(convertToInt),
+		Title:   body.Title,
+		Content: body.Content,
+		Date:    body.Date,
+	}
+	return c.JSON(response)
 }
